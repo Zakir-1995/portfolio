0c7d527e4646fd823b353dae0f7aaed016c73d8f
Md Zakir Hossain <58882834+Zakir-1995@users.noreply.github.com>
2024-12-31 19:07:22 +0600

    Add files via upload

---
+++ frontend/src/Router.jsx
@@ -0,0 +1,48 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "./layout/Main";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Blog from "./pages/Blog"
+import CreateBlog from "./pages/CreateBlog";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/create-blog",
+        element: <CreateBlog />,
+      },
+    ],
+  },
+]);
+
+export default router;
---
+++ frontend/src/components/Pin.jsx
@@ -0,0 +1,28 @@
+import { Marker, Popup } from "react-leaflet";
+import PropTypes from "prop-types";
+const Pin = ({ item }) => {
+  const position = [item.latitude, item.longitude];
+  return (
+    <Marker position={position}>
+      <Popup>
+        <div className="w-full ">
+          <img
+            src={item.img}
+            alt={item.title}
+            className="w-full rounded-md object-cover"
+          />
+          <h5 className="text-xs font-bold  mt-2">{item.title}</h5>
+          <span className="text-[12px] font-light text-masala ">
+            {item.address}
+          </span>
+        </div>
+      </Popup>
+    </Marker>
+  );
+};
+
+export default Pin;
+
+Pin.propTypes = {
+  item: PropTypes.array,
+};
